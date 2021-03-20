abdc68c2f73aaa2f02fa0a661114ba26ede72990
willian Hilton <willian.hilton@hotmail.com>
2021-03-20 19:55:56 -0300

    feat: Inclusão da classe estado e comportamento

---
+++ src/OOP/01 - Classe x Objeto/Classe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP
+{
+    //Definição de classe
+    public class Carro
+    {
+        public Guid CarroId { get; set; }
+        public string Placa { get; set; }
+    }
+
+    public class Objeto
+    {
+        public Objeto()
+        {
+            //Definição de Objeto
+            var objeto = new Carro()
+            {
+                CarroId = Guid.NewGuid(),
+                Placa = "DXD-4180"
+            };
+        }
+    }
+}
---
+++ src/OOP/02 - Pilares OOP/EstadoComportamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Pessoa
+    {
+        //Definição de Estado
+        public Guid PessoaId { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        //Definição de Comportamento
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+            return idade;
+        }
+    }
+}
