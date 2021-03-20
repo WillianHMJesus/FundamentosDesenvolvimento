3110e1bc0174ed04e32437d87617c6b9abdc89c5
willian Hilton <willian.hilton@hotmail.com>
2021-03-20 19:27:14 -0300

    feat: Inclusão da implementação Classe x Objeto

---
+++ src/OOP/Classe x Objeto/Classe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP.Classe_x_Objeto
+{
+    //Definição de classe
+    public class Classe
+    {
+        public Classe(string propriedade)
+        {
+            Propriedade = propriedade;
+        }
+
+        public string Propriedade { get; private set; }
+
+        public void Metodo(string parametro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/OOP/Classe x Objeto/Objeto.cs
@@ -0,0 +1,11 @@
+﻿namespace OOP.Classe_x_Objeto
+{
+    public class Objeto
+    {
+        public Objeto()
+        {
+            //Definição de objeto
+            var objeto = new Classe("");
+        }
+    }
+}
