0190973746072f298050ef885ed60a5b6a354c9b
willian Hilton <willian.hilton@hotmail.com>
2021-03-21 16:00:53 -0300

    feat: Implementação da Herança x Composição

---
+++ src/OOP/Heranca x Composicao/Casos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OOP
+{
+    public class PessoaHeranca : Pessoa
+    {
+        public string Cpf { get; set; }
+    }
+
+    public class PessoaComposicao
+    {
+        public Pessoa Pessoa { get; set; }
+        public string Cpf { get; set; }
+    }
+
+    public class UsoHerancaComposicao
+    {
+        public UsoHerancaComposicao()
+        {
+            var pessoaHeranca = new PessoaHeranca()
+            {
+                PessoaId = Guid.NewGuid(),
+                Nome = "José dos Santos",
+                DataNascimento = DateTime.Now.AddYears(-18),
+                Cpf = "66876628051"
+            };
+
+            var pessoaComposicao = new PessoaComposicao()
+            {
+                Pessoa = new Pessoa()
+                {
+                    PessoaId = Guid.NewGuid(),
+                    Nome = "José dos Santos",
+                    DataNascimento = DateTime.Now.AddYears(-18)
+                },
+                Cpf = "66876628051"
+            };
+
+            var nomeHeranca = pessoaHeranca.Nome;
+            var nomeComposicao = pessoaComposicao.Pessoa.Nome;
+        }
+    }
+}
