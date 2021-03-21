902f291199f58fef4cfbd6dc665e1f9b03693135
willian Hilton <willian.hilton@hotmail.com>
2021-03-21 15:33:44 -0300

    feat: Implementação da Interface x Implementação

---
+++ src/OOP/03 - Interface x Implementacao/Casos.cs
@@ -0,0 +1,56 @@
+﻿namespace OOP
+{
+    public interface IRepositorio
+    {
+        void Adicionar();
+    }
+
+    public class Repositorio : IRepositorio
+    {
+        public void Adicionar() { }
+    }
+
+    public class RepositorioFake : IRepositorio
+    {
+        public void Adicionar() { }
+    }
+
+    public class UsoImplementacao
+    {
+        public void Processo()
+        {
+            var repositorio = new Repositorio();
+            repositorio.Adicionar();
+        }
+    }
+
+    public class UsoInterface
+    {
+        private readonly IRepositorio _repositorio;
+
+        public UsoInterface(IRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public void Processo()
+        {
+            _repositorio.Adicionar();
+        }
+    }
+
+    public class UsoImplementacaoInterface
+    {
+        public UsoImplementacaoInterface()
+        {
+            var implementacao = new UsoImplementacao();
+            implementacao.Processo();
+
+            var abstracao = new UsoInterface(new Repositorio());
+            abstracao.Processo();
+
+            var abstracaoFake = new UsoInterface(new RepositorioFake());
+            abstracaoFake.Processo();
+        }
+    }
+}
