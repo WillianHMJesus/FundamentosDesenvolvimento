0d2c69ba8d0bd3bb6421b7d01494b5f747b8bbaf
willian Hilton <willian.hilton@hotmail.com>
2021-03-21 15:15:26 -0300

    feat: Implementação da abstração

---
+++ src/OOP/02 - Pilares OOP/Abstracao.cs
@@ -0,0 +1,11 @@
+﻿namespace OOP
+{
+    public abstract class Conta
+    {
+        public decimal Saldo { get; protected set; }
+
+        public abstract void Debitar(decimal valor);
+
+        public void Depositar(decimal valor) => Saldo += valor;
+    }
+}
