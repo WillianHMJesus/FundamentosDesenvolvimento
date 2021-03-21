fe2ce85e1c213c93902d4889317751c3f321a4fe
willian Hilton <willian.hilton@hotmail.com>
2021-03-20 21:07:26 -0300

    feat: Implementação da funcionalidade de Herança

---
+++ src/OOP/02 - Pilares OOP/Herança.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Funcionario : Pessoa
+    {
+        public DateTime DataAdmissao { get; set; }
+        public string Registro { get; set; }
+    }
+
+    public class Processo
+    {
+        public void Execucao()
+        {
+            var funcionario = new Funcionario()
+            {
+                Nome = "Mario Jorge da Silva",
+                DataNascimento = new DateTime(1990, 09, 20),
+                DataAdmissao = DateTime.Now,
+                Registro = "10102050"
+            };
+            var idade = funcionario.CalcularIdade();
+        }
+    }
+}
