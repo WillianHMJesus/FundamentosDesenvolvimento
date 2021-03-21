539c2542bdfb85cadc6548e22ad8fe2495fc99a5
willian Hilton <willian.hilton@hotmail.com>
2021-03-21 15:16:34 -0300

    feat: Implementação do encapsulamento

---
+++ src/OOP/02 - Pilares OOP/Encapsulamento.cs
@@ -0,0 +1,16 @@
+﻿namespace OOP
+{
+    public class Transacao
+    {
+        public Transacao()
+        {
+            Conta contaCorrente = new ContaCorrente();
+            contaCorrente.Depositar(100);
+            contaCorrente.Debitar(100);
+
+            Conta contaPoupanca = new ContaPoupanca();
+            contaPoupanca.Depositar(150);
+            contaPoupanca.Debitar(150);
+        }
+    }
+}
