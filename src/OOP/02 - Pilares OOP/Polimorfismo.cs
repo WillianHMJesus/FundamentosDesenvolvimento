78ebdff5f6cf260a2fdbfef65ba87ad7a98b45c0
willian Hilton <willian.hilton@hotmail.com>
2021-03-21 15:16:07 -0300

    feat: Implementação do polimorfismo

---
+++ src/OOP/02 - Pilares OOP/Polimorfismo.cs
@@ -0,0 +1,36 @@
+﻿namespace OOP
+{
+    public class ContaCorrente : Conta
+    {
+        private bool ValidarDebito()
+        {
+            //Implementar regra de débito de conta corrente
+            return true;
+        }
+
+        public override void Debitar(decimal valor)
+        {
+            if (ValidarDebito())
+            {
+                Saldo -= valor;
+            }
+        }
+    }
+
+    public class ContaPoupanca : Conta
+    {
+        private bool ValidarDebito()
+        {
+            //Implementar regra de débito de conta poupança
+            return true;
+        }
+
+        public override void Debitar(decimal valor)
+        {
+            if(ValidarDebito())
+            {
+                Saldo -= valor;
+            }
+        }
+    }
+}
